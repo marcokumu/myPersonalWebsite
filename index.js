bddce08efb02845ac8343163ba32f3628fbcab57
Okumu Mark <marcokumu10@gmail.com>
2022-06-07 17:01:40 +0300

    video introduction

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const inputName = document.querySelector(".name");
 
-function myFunction() {
-    var name = document.getElementById("fullname");
-    name.value = name.value.toUppercase();
-}
+const inputEmail = document.querySelector(".email");
+
+const inputMessage = document.querySelector(".message");
+
+
+inputName.addEventListener("click", function() {
+alert ('You have clicked me!')
+})
+
+inputEmail.addEventListener("click", function() {
+    alert('You have clicked me!')
+} )
+
+inputMessage.addEventListener("click", function() {
+    alert('You have clicked me!')
+} )
+
+
+const hello = document.querySelector(".hello");
+
+hello.addEventListener("click", function() {
+    hello.innerHTML = 'Welcome'
+} )
